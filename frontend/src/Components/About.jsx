2ce0d64ee61861695f04ea37fcaa212c389cfc72
Muaaz Ahmad <muaaz.ahmad042@gmail.com>
2024-11-25 13:39:39 +0500

    About Section completed

--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div id='about' className='flex flex-col justify-center gap-5 md:px-24 px-7 md:py-20 py-10'>
             <h1 className='md:text-8xl text-5xl text-center'>About</h1>
-            <p>
+            <p className='text-justify'>
                 Welcome to Your <span className='font-semibold'>AI-Powered Kitchen Assistant,</span> a revolutionary tool that bridges the gap between creativity and convenience in cooking. Whether you're a seasoned chef, an experimental foodie, or someone just starting their culinary journey, this platform is here to transform the way you approach the kitchen.
             </p>
             <h2 className='text-xl font-bold'>With cutting-edge AI technology, our platform offers two powerful features:</h2>
@@ -19,9 +19,8 @@ const About = () => {
                 <li className='ms-8'><span className='font-semibold'>Personalized Experience:</span> Designed to make cooking accessible for everyone, regardless of experience level.</li>
                 <li className='ms-8'><span>Seamless Design:</span> A user-friendly interface ensures that you can focus on cooking rather than navigating through complexities.</li>
             </ul>
-            <p>
-                This platform is more than a recipe generator—it's your personal sous-chef, powered by artificial intelligence. Whether you're inspired by a dish you saw at a restaurant, or you want to explore new recipes from the comfort of your home, this tool makes it possible. <br />
-                Embark on a culinary adventure today with your <span className='font-semibold'>AI-Powered Kitchen Assistant</span>—where technology meets taste!
+            <p className='text-justify'>
+                This platform is more than a recipe generator—it's your personal sous-chef, powered by artificial intelligence. Whether you're inspired by a dish you saw at a restaurant, or you want to explore new recipes from the comfort of your home, this tool makes it possible. Embark on a culinary adventure today with your <span className='font-semibold'>AI-Powered Kitchen Assistant</span>—where technology meets taste!
             </p>
         </div>
     )
