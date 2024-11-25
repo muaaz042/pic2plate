76532d3ad8f52493423324320f57bcb52109a6ba
Muaaz Ahmad <muaaz.ahmad042@gmail.com>
2024-11-25 13:43:05 +0500

    Logo Setting

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -10,8 +10,8 @@ const Header = () => {
     <div>
       <div className='flex justify-between items-center w-full lg:px-16 md:px-10 sm:px-8 px-4 py-2 shadow-lg'>
         <div className='flex items-center gap-2'>
-          <img src="logo.png" alt="logo" className='h-20 w-28' />
-          <p className='text-xl font-bold'>pic<span className='text-orange text-2xl'>2</span>plate</p>
+          <img src="logo.png" alt="logo" className='md:h-20 md:w-28 sm:h-14 sm:w-20 h-10 w-14' />
+          <p className='md:text-xl text-lg font-bold'>pic<span className='text-orange text-2xl'>2</span>plate</p>
         </div>
         <div className='md:flex items-center gap-6 hidden'>
           <a href="#home" className='font-medium hover:text-orange hover:scale-110'>Home</a>
