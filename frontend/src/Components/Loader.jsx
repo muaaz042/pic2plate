dc1d47d29ed035f80465a09ecb6308a8ddea4f77
Muaaz Ahmad <muaaz.ahmad042@gmail.com>
2024-11-25 17:02:20 +0500

    Adding Loader

--- frontend/src/Components/Loader.jsx
+++ frontend/src/Components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loader = () => {
     return (
-        <div class="text-center mt-8">
+        <div class="text-center my-8">
             <div
                 class="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-orange mx-auto"
             ></div>
