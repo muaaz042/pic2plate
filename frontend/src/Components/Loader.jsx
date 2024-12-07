e5f64246f22e64094dba58101c17f86faf0ad609
Muaaz Ahmad <muaaz.ahmad042@gmail.com>
2024-12-07 23:55:21 +0500

    Updating loader

--- frontend/src/Components/Loader.jsx
+++ frontend/src/Components/Loader.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Loader = () => {
     return (
-        <div class="text-center my-8">
-            <div
-                class="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-orange mx-auto"
-            ></div>
-            <h2 class="text-black mt-4">Please wait .....</h2>
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+            <div className="text-center">
+                <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-orange mx-auto"></div>
+                <h2 className="text-white mt-4">Please wait .....</h2>
+            </div>
         </div>
     )
 }
