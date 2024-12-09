6aeffc7fb6ab6f514c7079f18f1a01a1f6b71731
Muaaz Ahmad <muaaz.ahmad042@gmail.com>
2024-12-09 14:35:45 +0500

    Custom model added

--- MobileNetV2/main.py
+++ MobileNetV2/main.py
@@ -5,7 +5,7 @@ import numpy as np
 from tensorflow.keras.preprocessing.image import img_to_array
 from keras.models import load_model
 
-# Preprocessing function
+
 def preprocess_image(image, target_size):
     image = image.resize(target_size)
     image_array = img_to_array(image)
@@ -13,7 +13,7 @@ def preprocess_image(image, target_size):
     image_array = np.expand_dims(image_array, axis=0)
     return image_array
 
-# Prediction function
+
 def predict_image_from_url(image_url, model, target_size, selected_classes) -> str:
     response = requests.get(image_url)
     if response.status_code != 200:
@@ -25,11 +25,12 @@ def predict_image_from_url(image_url, model, target_size, selected_classes) -> s
     dish = selected_classes[np.argmax(prediction)]
     return dish
 
-# Load the trained model
-model_path = "./recipe-detector.keras"  # Update this path if your model is in a different location
+
+model_path = "./recipe-detector.keras" 
 model = load_model(model_path)
 
-# Define the target size and the class labels
+
+
 target_size = (256, 256)
 selected_classes = [
     "Achari Gosht", "Aloo Gosht", "Aloo Ki Bhujia", "Anday Wala Burger",
@@ -39,9 +40,9 @@ selected_classes = [
     "Dahi Bhalla", "Falooda", "Fish Tikka", "Gajar Ka Halwa"
 ]
 selected_classes = selected_classes[:10]
-# Example image URL for prediction
+
 image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoDBt2CECWHLvV2bqtTYsSkB85Irmft5lNtQ&s"
 
-# Predict and print the label
+
 predicted_label = predict_image_from_url(image_url, model, target_size, selected_classes)
 print(f"Predicted label: {predicted_label}")
